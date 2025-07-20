06e937c3bba90beff48c432509af6222f4f228f7
jekrch <jekrch@gmail.com>
2025-07-19 19:06:14 -0500

    fix border

--- src/components/ScoreTrackBorder.tsx
+++ src/components/ScoreTrackBorder.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { ScoreDot } from './ScoreDot';
 import { useScorePositions } from '../hooks/useScorePositions';
+import { useDimensions } from '../hooks/useDimensions';
 import type { Player } from '../types';
+
 
 interface ScoreTrackBorderProps {
   players: Player[];
 }
 
+
 export const ScoreTrackBorder: React.FC<ScoreTrackBorderProps> = ({ players }) => {
   const { getScorePosition } = useScorePositions();
+  const { windowDimensions } = useDimensions();
+
 
   return (
     <div className="absolute inset-0 z-[70] pointer-events-none">
@@ -17,7 +22,8 @@ export const ScoreTrackBorder: React.FC<ScoreTrackBorderProps> = ({ players }) =
       <div className="absolute bottom-0 left-0 right-0 h-8 bg-znr-elevated"></div>
       <div className="absolute top-0 bottom-0 left-0 w-6 bg-znr-elevated"></div>
       <div className="absolute top-0 bottom-0 right-0 w-6 bg-znr-elevated"></div>
-      
+
+
       {/* Score markers */}
       <div className="absolute inset-0">
         {Array.from({ length: 100 }, (_, i) => {
@@ -35,10 +41,35 @@ export const ScoreTrackBorder: React.FC<ScoreTrackBorderProps> = ({ players }) =
             />
           );
         })}
-        
-        {/* Score number labels */}
+
+
+        {/* Score number labels for corner positioning */}
         {[0, 25, 50, 75].map(score => {
-          const pos = getScorePosition(score);
+          const { width, height } = windowDimensions;
+          const borderWidth = 24; 
+          const borderHeight = 32; 
+
+
+          let pos;
+          switch (score) {
+            case 0: // Top-Left Corner
+              pos = { x: borderWidth / 2, y: borderHeight / 2 };
+              break;
+            case 25: // Top-Right Corner
+              pos = { x: width - (borderWidth / 2), y: borderHeight / 2 };
+              break;
+            case 50: // Bottom-Right Corner
+              pos = { x: width - (borderWidth / 2), y: height - (borderHeight / 2) };
+              break;
+            case 75: // Bottom-Left Corner
+              pos = { x: borderWidth / 2, y: height - (borderHeight / 2) };
+              break;
+            default:
+              pos = getScorePosition(score);
+              break;
+          }
+
+
           return (
             <div
               key={score}
@@ -54,7 +85,8 @@ export const ScoreTrackBorder: React.FC<ScoreTrackBorderProps> = ({ players }) =
           );
         })}
       </div>
-      
+
+
       {/* Player score dots */}
       {players.map(player => (
         <ScoreDot
